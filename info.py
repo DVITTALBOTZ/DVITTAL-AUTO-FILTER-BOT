4666859073885abb426234e4c797e10585be3412
Padimallikarjun23 <paditriveni1999@gmail.com>
2025-09-05 13:59:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'dreamxbotz_search')   # Session name for the bot
 API_ID = int(environ.get('API_ID', '22262560')) # API ID from my.telegram.org
 API_HASH = environ.get('API_HASH', '73eeccd990484d0c87a90756aae9fa21')  # API Hash from my.telegram.org
-BOT_TOKEN = environ.get('BOT_TOKEN', "")    # Bot token from @BotFather
+BOT_TOKEN = environ.get('BOT_TOKEN', "8302871737:AAF7s6Na08fjLIiv2u9tMM2nu1EtucxqqyY")    # Bot token from @BotFather
 
 # ============================
 # Bot Settings Configuration
@@ -120,8 +120,8 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "5400"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+brzMARqMRpA5N2M1') # Group link for the bot
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/M4U_Admin_Bot') # Owner link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+yYiKU0x6eGw3OGU1') # Group link for the bot
+OWNER_LNK = environ.get('OWNER_LNK', 'http://t.me/Malli4U_Admin_Bot') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/Malli4U_Official2') # Update channel link for the bot
 
 # ============================
