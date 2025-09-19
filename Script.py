2f93bb3e1f7d01e1792a6942c5d46cb62fbf826c
Flix_botz <99806982+Priyanshu1057@users.noreply.github.com>
2025-09-19 16:43:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -230,7 +230,7 @@ Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
 <code>{filename}</code>
 <b>(Touch to copy)</b>
  
-<pre><b>▶ ᴀᴜᴅɪᴏ ᴛʀᴀᴄᴋ  : 🔊 {tags}{language}</b></pre>
+<pre><b>▶ ᴀᴜᴅɪᴏ ᴛʀᴀᴄᴋ  : 🔊 {language}</b></pre>
 
 </b><a href={poster_url}></a>▶ <a href={imdb_url}>ᴅᴇᴛᴀɪʟꜱ 📋
 </a></b>
