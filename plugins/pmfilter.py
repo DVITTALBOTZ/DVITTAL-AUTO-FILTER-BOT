d84b3435113fc82a877f9706a8fd150d5ad8e101
UNKOWN5757H <65911268+UNKOWN5757H@users.noreply.github.com>
2025-09-30 20:21:56 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -433,30 +433,31 @@ async def next_page(bot, query):
 
 @Client.on_callback_query(filters.regex(r"^spol"))
 async def advantage_spoll_choker(bot, query):
-    _, id, user = query.data.split("#")
+    _, id, user = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer(
-            script.ALRT_TXT.format(query.from_user.first_name), show_alert=True
-        )
-
+        return await query.answer(script.ALRT_TXT.format(query.from_user.first_name), show_alert=True)
     movies = await get_poster(id, id=True)
-    movie = movies.get("title")
+    movie = movies.get('title')
     movie = re.sub(r"[:-]", " ", movie)
     movie = re.sub(r"\s+", " ", movie).strip()
-
     await query.answer(script.TOP_ALRT_MSG)
-
-    files, offset, total_results = await get_search_results(
-        query.message.chat.id, movie, offset=0, filter=True
-    )
-
+    files, offset, total_results = await get_search_results(query.message.chat.id, movie, offset=0, filter=True)
     if files:
         k = (movie, files, offset, total_results)
         await auto_filter(bot, query, k)
     else:
-        # 🚫 Just ignore silently (do nothing)
-        return
-
+        reqstr1 = query.from_user.id if query.from_user else 0
+        reqstr = await bot.get_users(reqstr1)
+        if NO_RESULTS_MSG:
+            try:
+                await bot.send_message(chat_id=BIN_CHANNEL, text=script.NORSLTS.format(reqstr.id, reqstr.mention, movie))
+            except Exception as e:
+                print(f"Error In Spol - {e}   Make Sure Bot Admin BIN CHANNEL")
+        btn = InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🔰Cʟɪᴄᴋ ʜᴇʀᴇ & ʀᴇǫᴜᴇsᴛ ᴛᴏ ᴀᴅᴍɪɴ🔰", url=OWNER_LNK)]])
+        k = await query.message.edit(script.MVE_NT_FND, reply_markup=btn)
+        await asyncio.sleep(10)
+        await k.delete()
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
